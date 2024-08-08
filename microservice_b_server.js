a77da9d7aaa8f6d627654eda11c5f9b2afde5bd8
William Chen <williamchen120@gmail.com>
2024-08-07 23:21:31 -0700

    Added functionality

--- microservice_b_server.js
+++ microservice_b_server.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 300;
+const PORT = 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 
-console.log(process.env.API_key)
+async function aqiData(lat, lon, key) {
 
-async function aqiData(lat, long) {
-
-    let response = await fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=1&lon=1&appid=`);
+    let response = await fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}1&appid=${key}`);
     let data = await response.json();
     let jsonData = {
-        'AQI': data["list"][0]['main']['aqi'],
-        'Components': data["list"][0]['components']
+        'aqi': data["list"][0]['main']['aqi'],
+        'components': data["list"][0]['components']
     }
 
     return jsonData
 };
 
-aqiData(1, 1).then((data) => console.log(data))
+app.post("/aqiData", (req, res) => {
+    aqiData(req.body["lat"], req.body["lon"], req.body["key"]).then((data) =>
+        res.send(data))
+})
 
+app.listen(PORT, () => {
+    console.log(`Litsening on port ${PORT}`)
+});
